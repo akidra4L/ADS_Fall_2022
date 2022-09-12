92c0b8aed0ea6468532b191f1ed35aff3e1d7c99
Alikhan Gubayev <jonasofan.gubaev@gmail.com>
2022-09-12 19:31:44 +0600

    ads

--- lab01/i.cpp
+++ lab01/i.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
 int main() {
     int n;
-    string s;
-    cin >> n >> s;
+    string str;
+    cin >> n >> str;
+
+    queue <int> k, s;
+    for(int i = 0; i < str.size(); i++) {
+        if(str[i] == 'K') {
+            k.push(i);  
+        } else {
+            s.push(i);
+        }
+    }
+
+    while(!k.empty() && !s.empty()) {
+        int kat = k.front();
+        int sak = s.front();
+        k.pop();
+        s.pop();
+
+        if(kat < sak) {
+            k.push(kat + n);
+        } else {
+            s.push(sak + n);
+        }
+    }
+
+    if(!k.empty()) {
+        cout << "KATSURAGI" << endl;
+    } else {
+        cout << "SAKAYANAGI" << endl;
+    }
 
     return 0;
 }
