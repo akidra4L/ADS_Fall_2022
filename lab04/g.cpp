629899800546f52c5a787bbcc8cf977a05af7f07
Alikhan Gubayev <jonasofan.gubaev@gmail.com>
2022-10-05 00:53:02 +0600

    lab-04

--- lab04/g.cpp
+++ lab04/g.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <algorithm>
 #include <queue>
-#include <vector>
 
 using namespace std;
 
@@ -17,7 +16,6 @@ struct Node {
 struct BST {
     private:
         Node *root;
-        int distance;
 
         Node* _insert(Node *cur, int value) {
             if(!cur) {
@@ -31,91 +29,46 @@ struct BST {
             return cur;
         }
 
-        Node* _getDeepestElement(Node *cur) {
-            if(!cur) {
-                return nullptr;
+        int _maxDepth(Node* node) {
+            if(!node) {
+                return 0;
             }
-
-            Node *tmp = nullptr;
-            queue <Node*> q;
-            q.push(cur);
-            while(!q.empty()) {
-                tmp = q.front();
-                q.pop();
-                if(tmp->left) {
-                    q.push(tmp->left);
-                } 
-                if(tmp->right) {
-                    q.push(tmp->right);
-                }
-            }
-            return tmp;
+            int leftDepth = _maxDepth(node->left);
+            int rightDepth = _maxDepth(node->right);
+            return max(leftDepth, rightDepth) + 1;
         }
 
-        int _findDistance(Node* cur, int n1, int n2) {
-            if(!cur) {
-                return 0;
-            }
-            int left = _findDistance(cur->left, n1, n2);
-            int right = _findDistance(cur->right, n1, n2);
-            if(cur->value == n1 || cur->value == n2) {
-                if(left || right) {
-                    distance = max(left, right);
-                    return 0;
-                } else {
-                    return 1;
+        int _getDistance(Node *node, int max) {
+            Node *cur = node;
+            queue <Node*> q;
+            while(!q.empty() || cur) {
+                while(cur) {
+                    q.push(cur);
+                    cur = cur->left;
                 }
-            } else if(left && right) {
-                distance = left + right;
-                return 0;
-            } else if(left || right) {
-                return max(left, right) + 1;
+                if(!q.empty() && !cur) {
+                    int distance = _maxDepth(q.front()->left) + _maxDepth(q.front()->right) + 1;
+                    if(max < distance) {
+                        max = distance;
+                    }
+                    cur = q.front()->right;
+                    q.pop();
+                }
             }
-            return 0;
+            return max;
         }
 
     public:
         BST() {
             this->root = nullptr;
-            this->distance = 0;
         }
 
         void insert(int value) {
             root = _insert(root, value);
         }
 
-        vector <Node*> getLeaves() {
-            vector <Node*> leafes;
-            queue <Node*> q;
-            q.push(root);
-            while(!q.empty()) {
-                Node* cur = q.front();
-                q.pop();
-                if(!cur->left && !cur->right) {
-                    leafes.push_back(cur);
-                }
-                if(cur->left) {
-                    q.push(cur->left);
-                }
-                if(cur->right) {
-                    q.push(cur->right);
-                }
-            }
-            return leafes;
-        }
-
-        int findDistance(int n1, int n2) {
-            distance = 0;
-            _findDistance(root, n1, n2);
-            return distance + 1;
-        }
-
-        Node* getDeepestElement() {
-            return _getDeepestElement(root);
-        }
-
-        Node* getRoot() {
-            return this->root;
+        int getDistance(int max) {
+            return _getDistance(root, max);
         }
 };
 
@@ -129,15 +82,7 @@ int main() {
         bst->insert(x);
     }
 
-    Node *deepest = bst->getDeepestElement();
-    vector <Node*> leaves = bst->getLeaves();
-    
-    vector <int> distances;
-    distances.push_back(bst->findDistance(deepest->value, bst->getRoot()->value));
-    for(int i = 0; i < leaves.size(); i++) {
-        distances.push_back(bst->findDistance(deepest->value, leaves[i]->value));
-    }
-    cout << *max_element(distances.begin(), distances.end()) << "\n";
+    cout << bst->getDistance(-1) << "\n";
 
     return 0;
 }
