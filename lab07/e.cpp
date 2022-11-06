871cb3b80d7294245c1cc2719c65bb171dc51338
Alikhan Gubayev <jonasofan.gubaev@gmail.com>
2022-11-06 15:22:32 +0600

    lab-07

--- lab07/e.cpp
+++ lab07/e.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <numeric>
 
 using namespace std;
 
@@ -9,8 +8,9 @@ vector <vector <int> > merge(vector <vector <int> > left, vector <vector <int> >
     int l = 0, r = 0;
     vector <vector <int> > result;
     while(l < left.size() && r < right.size()) {
-        int left_sum = accumulate(left[l].begin(), left[l].end(), 0);
-        int right_sum = accumulate(right[r].begin(), right[r].end(), 0); 
+
+        int left_sum = left[l][left[l].size() - 1];
+        int right_sum = right[r][right[r].size() - 1];
         if(left_sum < right_sum) {
             result.push_back(right[r]);
             r++;
@@ -18,7 +18,8 @@ vector <vector <int> > merge(vector <vector <int> > left, vector <vector <int> >
             result.push_back(left[l]);
             l++;
         }  else {
-            for(int i = 0; i < left[l].size(); i++) {
+            int cnt = 0;
+            for(int i = 0; i < left[l].size() - 1; i++) {
                 if(left[l][i] < right[r][i]) {
                     result.push_back(left[l]);
                     l++;
@@ -28,6 +29,11 @@ vector <vector <int> > merge(vector <vector <int> > left, vector <vector <int> >
                     r++;
                     break;
                 }
+                cnt++;
+            }
+            if(cnt == left[l].size() - 1) {
+                result.push_back(left[l]);
+                l++;
             }
         }
     }
@@ -41,7 +47,6 @@ vector <vector <int> > merge(vector <vector <int> > left, vector <vector <int> >
     }
     return result;
 }
-
 vector <vector <int> > mergeSort(vector <vector <int> > a, int l, int r) {
     if(l == r) {
         vector <vector <int> > result;
@@ -53,20 +58,28 @@ vector <vector <int> > mergeSort(vector <vector <int> > a, int l, int r) {
     vector <vector <int> > right = mergeSort(a, m + 1, r);
     return merge(left, right);
 }
+
 int main() {
     int n, m;
     cin >> n >> m;
-    vector<vector<int>> a(n);
+    vector <vector <int> > a;
     for(int i = 0; i < n; i++) {
-        a[i] = vector <int>(m);
+        vector <int> tmp;
+        int total = 0;
         for(int j = 0; j < m; j++) {
-            cin >> a[i][j];
+            int x; cin >> x;
+            tmp.push_back(x);
+            total += x;
         }
+        tmp.push_back(total);
+        a.push_back(tmp);
     }
+
     vector <vector <int> > result = mergeSort(a, 0, n - 1);
+
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            cout << result[i][j]<<" ";
+            cout << result[i][j] << " ";
         }
         cout << "\n";
     }
